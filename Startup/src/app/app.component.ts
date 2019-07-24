252ca4abe8e6cb9d969dd5acd0e8bb8414c4a4c9
SaiWannaAung1 <saiwannaaung095@gmail.com>
2019-07-24 21:52:14 +0630

    Initial-Commit

---
+++ Startup/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'] // can used multiple css
+})
+export class AppComponent {
+  title = 'Startup';
+  name = ['Sai Wanna Aung','Robin','Maisoneka','Mr Rock'];
+}
---
+++ Startup/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { NaviComponent } from './navi/navi.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NaviComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
